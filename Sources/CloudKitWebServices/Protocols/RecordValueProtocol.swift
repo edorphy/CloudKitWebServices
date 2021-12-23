f2f16e751fb0fcd6bb0b13d6c6d56eec98734410
Eric Dorphy <edorphy@icloud.com>
2021-12-23 11:01:21 -0600

    - Reordered RecordValueType conformance extensions to match the ordering of the reference - Added encoding support for data, date, double, and location

--- Sources/CloudKitWebServices/Protocols/RecordValueProtocol.swift
+++ Sources/CloudKitWebServices/Protocols/RecordValueProtocol.swift
@@ -10,20 +10,20 @@ import CoreLocation
 
 public protocol RecordValueProtocol { }
 
+// Refer to the Types and Dictionaries section within the reference
+// https://developer.apple.com/library/archive/documentation/DataManagement/Conceptual/CloudKitWebServicesReference/Types.html
+
+extension Data: RecordValueProtocol { }
 extension Date: RecordValueProtocol { }
 extension Double: RecordValueProtocol { }
 extension Int64: RecordValueProtocol { }
 
+extension CLLocation: RecordValueProtocol { }
+extension LocationDictionary: RecordValueProtocol { }
+
 extension Record.Reference: RecordValueProtocol { }
+extension ReferenceDictionary: RecordValueProtocol { }
 
 extension String: RecordValueProtocol { }
-extension CLLocation: RecordValueProtocol { }
 
 extension Array: RecordValueProtocol where Element: RecordValueProtocol { }
-
-// MARK: - Internal Types
-
-// TODO: Consider making a RecordValueProtocolInternal that inherits from RecordValue protocol
-
-extension LocationDictionary: RecordValueProtocol { }
-extension ReferenceDictionary: RecordValueProtocol { }
--- Sources/CloudKitWebServices/Support/Dictionaries/RecordFieldDictionary.swift
+++ Sources/CloudKitWebServices/Support/Dictionaries/RecordFieldDictionary.swift
@@ -16,6 +16,8 @@ struct RecordFieldDictionary: Codable {
     }
     
     let value: RecordValueProtocol
+    
+    // TODO: The reference states that type is optional, but what is the practical usage of this? Maybe make it required
     let type: String?
     
     init(value: RecordValueProtocol, type: String?) {
@@ -28,17 +30,39 @@ struct RecordFieldDictionary: Codable {
         try container.encode(type, forKey: .type)
         
         switch value {
+            
+        case let dataValue as Data:
+            let base64Encodedvalue = dataValue.base64EncodedString()
+            try container.encode(base64Encodedvalue, forKey: .value)
+            
+        case let dateValue as Date:
+            // Represented as a number in milliseconds between midnight on January 1, 1970, and the specified date or time.
+            let epochValue = dateValue.timeIntervalSince1970 * 1000
+            try container.encode(epochValue, forKey: .value)
+            
+        case let doubleValue as Double:
+            try container.encode(doubleValue, forKey: .value)
+            
         case let intValue as Int64:
             try container.encode(intValue, forKey: .value)
             
-        case let stringValue as String:
-            try container.encode(stringValue, forKey: .value)
+        case let locationValue as CLLocation:
+            let locationDictionary = LocationDictionary(from: locationValue)
+            try container.encode(locationDictionary, forKey: .value)
+            
+        case let locationValue as LocationDictionary:
+            try container.encode(locationValue, forKey: .value)
             
         case let referenceValue as ReferenceDictionary:
             try container.encode(referenceValue, forKey: .value)
             
         case let recordReferenceValue as Record.Reference:
             try container.encode(ReferenceDictionary(reference: recordReferenceValue), forKey: .value)
+            
+        case let stringValue as String:
+            try container.encode(stringValue, forKey: .value)
+            
+        // TODO: Add support for array encoding
             
         default:
             fatalError("RecordFieldDictionary encode not implemented for type \(type ?? "unspecified-type")")
