d1bf1bb6c018ab0bb136e7c7f3f6e12a39b83a3c
Eric Dorphy <edorphy@icloud.com>
2021-12-26 20:04:19 -0600

    #8 - Added basic support for exposing Asset downloadURL to public API

---
+++ Sources/CloudKitWebServices/Asset/CKWSRemoteAsset.swift
@@ -0,0 +1,23 @@
+//
+//  CKWSRemoteAsset.swift
+//  CloudKitWebServices
+//
+//  Created by Eric Dorphy on 12/26/21.
+//  Copyright © 2021 Twin Cities App Dev LLC. All rights reserved.
+//
+
+import Foundation
+
+public struct CKWSRemoteAsset: CKWSAssetProtocol {
+    public let downloadURL: URL
+    
+    init(assetDictionary: AssetDictionary) {
+        guard let downloadURL = assetDictionary.downloadURL else {
+            fatalError("Constructing a remote asset without a downloadURL - huh?")
+        }
+        
+        self.downloadURL = downloadURL
+    }
+}
+
+extension CKWSRemoteAsset: CKWSRecordValueProtocol { }
---
+++ Sources/CloudKitWebServices/Protocols/CKWSAssetProtocol.swift
@@ -0,0 +1,13 @@
+//
+//  CKWSAssetProtocol.swift
+//  CloudKitWebServices
+//
+//  Created by Eric Dorphy on 12/26/21.
+//  Copyright © 2021 Twin Cities App Dev LLC. All rights reserved.
+//
+
+import Foundation
+
+public protocol CKWSAssetProtocol {
+    
+}
